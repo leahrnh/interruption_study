7549b066bcc5095b98485c1c82833768e1ad7944
Leah <eahlay@gmail.com>
2015-11-26 10:30:42 -0500

    rewrote code for multiple modes, single touches

--- interruption.js
+++ interruption.js
@@ -1,136 +1,101 @@
 var score;
 var sessionID;
+var mode;
+
 var intro = new Audio('audio/intro.m4a'); //"Welcome! Today you be playing a game. I’m here to give you notifications. Go ahead and read the instructions on the right, then get started. Have fun!"
 var goodJob = new Audio('audio/good_job.m4a'); //"Good job"
 var tooLate = new Audio('audio/too_late.m4a'); //"Sorry. You're out of time."
 var almostDone = new Audio('audio/almost_done.m4a'); //"You have 1 more minute to play and finish any last tasks."
 
-var Item = function (name, siblings) {
+var excuseMe = new Audio('audio/excuseMe.m4a'); //Excuse me!
+var preUrgentPrompt = new Audio('audio/urgent.m4a'); //Urgent!
+var preRelaxPrompt = new Audio('audop/relax.m4a'); //When you have a minute
+
+var Item = function (name, prompt_base, prompt_urgent, prompt_relax) {
     this.name = name;
     this.touched = false;
     this.waiting = false;
-    this.siblings = siblings;
-    siblings.push(this);
+    //audio prompts
+    this.base = prompt_base; //ex. "Click on the phone"
+    this.urgent = prompt_urgent; //ex. "Click the phone right away"
+    this.relax = prompt_relax; //ex. "Click the phone at some point"
 }
 
-//1. 1 thing, right now
-var phoneTaskObjs = []; //"The phone is ringing. Answer it."
-var phone = new Item('phone', phoneTaskObjs);
+//TODO recreate/enlarge bed button
 
-//5. multiple things, right now
-var chopTaskObjs = []; //"It's time get the vegetables from the fridge and chop them, and put them in the pot for dinner"
-var fridge = new Item('fridge', chopTaskObjs);
-var cuttingboard = new Item('cuttingboard', chopTaskObjs);
-var knife = new Item('knife', chopTaskObjs);
-var littlepot = new Item('littlepot', chopTaskObjs);
+//big things
+var bed = new Item('bed', new Audio('audio/bedBase.m4a'),  new Audio('audio/bedUrgent.m4a'),  new Audio('audio/bedRelax.m4a'));
+var television = new Item('television', new Audio('audio/televisionBase.m4a'),  new Audio('audio/televisionUrgent.m4a'),  new Audio('audio/televisionRelax.m4a'));
+var stove = new Item('stove', new Audio('audio/stoveBase.m4a'),  new Audio('audio/stoveUrgent.m4a'),  new Audio('audio/stoveRelax.m4a'));
+var bathtub = new Item('bathtub', new Audio('audio/bathtubBase.m4a'),  new Audio('audio/bathtubUrgent.m4a'),  new Audio('audio/bathtubRelax.m4a'));
+var alarmclock = new Item('alarmclock', new Audio('audio/alarmclockBase.m4a'),  new Audio('audio/alarmclockUrgent.m4a'),  new Audio('audio/alarmclockRelax.m4a'));
+var encyclopedia = new Item('encyclopedia', new Audio('audio/encyclopediaBase.m4a'),  new Audio('audio/encyclopediaUrgent.m4a'),  new Audio('audio/encyclopediaRelax.m4a'));
+var toaster = new Item('toaster', new Audio('audio/toasterBase.m4a'),  new Audio('audio/toasterUrgent.m4a'),  new Audio('audio/toasterRelax.m4a'));
+var hairdryer = new Item('hairdryer', new Audio('audio/hairdryerBase.m4a'),  new Audio('audio/hairdryerUrgent.m4a'),  new Audio('audio/hairdryerRelax.m4a'));
 
-//6. 1 thing, soon
-var medicineTaskObjs = []; //"In the next two minutes, you need to take your medicine."
-var medicine = new Item('medicine', medicineTaskObjs);
-
-//3. multiple things, soon
-var dishwasherTaskObjs = []; //"In the next two minutes, empty the dishwasher and put the plates in the cabinet."
-var dishwasher = new Item('dishwasher', dishwasherTaskObjs);
-var cabinet = new Item('cabinet', dishwasherTaskObjs);
-
-//2. 1 thing, whenever
-var clockTaskObjs = []; //"At some point, be sure to wind the grandfather clock."
-var grandfatherclock = new Item('clock', clockTaskObjs);
-
-//4. multiple things, whenever
-var plantsTaskObjs = []; //At some point, make sure to water all the plants. There are 6 of them."
-var plant1 = new Item('plant1', plantsTaskObjs);
-var plant2 = new Item('plant2', plantsTaskObjs);
-var plant3 = new Item('plant3', plantsTaskObjs);
-var plant4 = new Item('plant4', plantsTaskObjs);
-var plant5 = new Item('plant5', plantsTaskObjs);
-var plant6 = new Item('plant6', plantsTaskObjs);
-
-//other future options
+//other phrase possibilities
 //  Wash your hair, then comb and blowdry it.
 //  "At some point today, be sure to make the bed and fluff the pillows."
 //  "Right now, stoke the fire."
-//  "The toast is ready! Get it before it burns!");
+//  The toast is ready! Get it before it burns!
+//  At some point, make sure to water all the plants. There are 6 of them."
+//  At some point, be sure to wind the grandfather clock."
+//  In the next two minutes, empty the dishwasher and put the plates in the cabinet.
+//  In the next two minutes, you need to take your medicine.
+//  It's time get the vegetables from the fridge and chop them, and put them in the pot for dinner
+//  The phone is ringing. Answer it.
 
 
-//
+//when entering from digit task, get session ID for use in logging
 function getSession(){
     var locate = window.location.toString();
     var point = locate.indexOf("=");
     sessionID = locate.substring(point+1,locate.length);
 }
 
+//start one round, with the appropriate settings
+//the lists should contain one of each object, and even numbers of urgencies (urgent or relaxed)
+function startRound(newMode, objectList, urgencyList) {
+    //some alert or something about starting the next round
+    score = 0;
+    mode = newMode;
+    var time = 0;
+    var status = 'startRound_' + newMode;
+    _LTracker.push({'session': sessionID,'event': status ,'score': score,});
+    for (var i=0; i<objectList.length; i++) {
+	time = time + 30000;
+	window.setTimeout(function() {
+	    obj = objectList[i];
+	    urgLevel = urgencyList[i];
+	    task(obj, urgLevel);
+	}, time);
+    }
+}
+
 //call different tasks at the appropriate time, with the appropriate initiation message
 function tasks() {
-    score = 0;
-    _LTracker.push({
-	'session': sessionID,
-	'event': 'initiate-game',
-	'score': score,
-});
+
+    //init
+    _LTracker.push({'session': sessionID,'event': 'initiate_game','score': score,});
     intro.play();
+    var time = 0;
+    time = time + 60000; //1 minute for reading instructions and getting started (and baseline game score?)
+
+    window.setTimeout(function() {
+	startRound('NoPre', [bed, toaster, television, hairdryer, alarmclock, encyclopedia, stove, bathtub], ['urgent', 'urgent', 'relax', 'urgent', 'relax', 'relax', 'urgent', 'relax']);
+    }, time);
     
-    //1 minute for reading instructions and getting started
-    
+    time = time + 300000; //5 minutes for a round
     window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'phonePrompt','score': score,});
-	task(phoneTaskObjs, 10000, new Audio('audio/phonePrompt.m4a'));
-	//"The phone is ringing. Answer it."
-    }, 60000);
+	startRound('PreBase', [hairdryer, television, alarmclock, stove, bathtub, toaster, bed, encyclopedia], ['relax', 'relax', 'urgent', 'relax', 'urgent', 'relax', 'urgent', 'urgent']);
+    }, time);
 
-    //10s for doing the task
-    //15s for playing the game
-
+    time = time + 300000; //5 minutes for a round
     window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'clockPrompt','score': score,});
-	task(clockTaskObjs, null, new Audio('audio/clockPrompt.m4a'));
-	//"At some point, be sure to wind the grandfather clock."
-    }, 85000);
-
-    //20s for playing the game
-
-    window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'dishwasherPrompt','score': score,});
-	task(dishwasherTaskObjs, 60000, new Audio('audio/dishwasherPrompt.m4a'));
-	//"In the next minute, empty the dishwasher and put the plates in the cabinet."
-    }, 105000);
-
-    //60s for accomplishing this task
-    //10s for transitioning (?)
-
-    window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'plantsPrompt','score': score,});
-	task(plantsTaskObjs, null, new Audio('audio/plantsPrompt.m4a'));
-	//At some point, make sure to water all the plants. There are 6 of them."
-    }, 175000);
-
-    //15s for playing the game
-    
-    window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'chopPrompt','score': score,});
-	task(chopTaskObjs, 10000, new Audio('audio/chopPrompt.m4a'));
-	//"It's time get the vegetables from the fridge and chop them, and put them in the pot for dinner"
-    }, 190000);
-
-    //10s for accomplishing the task
-    //20s for playing the game
-        
-    window.setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'medicinePrompt','score': score,});
-	task(medicineTaskObjs, 60000, new Audio('audio/medicinePrompt.m4a'));
-	//"In the next minute, you need to take your medicine."
-    }, 220000);
-
-    //40s buffer
-
-    setTimeout(function() {
-	_LTracker.push({'session': sessionID,'event': 'almostDonePrompt','score': score,});
-	almostDone.play();
-    }, 260000);
-    
-    //20s till task time is up
-    //40s till game time is up
-    
+	startRound('PreUrg', [alarmclock, hairdryer, bathtub, bed, television, toaster, encyclopedia, stove], ['urgent', 'relax', 'urgent', 'urgent', 'relax', 'relax', 'urgent', 'relax']);
+    }, time);
+  
+    time = time + 300000;
     setTimeout(function() {
 	_LTracker.push({'session': sessionID,'event': 'endOfSession','score': score,});
 	var tot = score + size - 1;
@@ -144,22 +109,50 @@ function tasks() {
 	$('#livingroom').hide();
 	$('#bathroom').hide();
 	alert("Time's up! Thank you for participating. Your final score was " + tot);
-    }, 300000);
+    }, time);
     
 }
 
-//perform a task that involves "touching" all the objects in the list in the given amount of time
-function task(objList, time, prompt) {
-    prompt.play();
-    for (var i=0; i<objList.length; i++) {
-	objList[i].touched = false;
-	objList[i].waiting = true;
+//perform a task that involves the specified object in the specified amount of time
+function task(obj, urgency) {
+
+    //decide how to play prompt it based on mode and urgency
+    if (mode=='NoPre') {
+	if (urgency=='urgent') {
+	    obj.urgent.play();
+	} else if (urgency =='relax') {
+	    obj.relax.play();
+	}
+    } else if (mode=='PreUrg') {
+	if (urgency=='urgent') {
+	    preUrgentPrompt.play();
+	} else {
+	    preRelaxPrompt.play();
+	}
+	window.setTimeout(function() {
+	    obj.base.play();
+	}, 10000);
     }
-    //if there is a time specified, the task must happen within that amount time. Otherwise, it can happen at any point
-    if (time!=null) {
+    else if (mode=='PreBase') {
+	excuseMe.play()
 	setTimeout(function() {
-	    checkTimeout(objList);
-	}, time)
+	    if (urgency=='urgent') {
+		obj.urgent.play();
+	    } else if (urgency =='relax') {
+		obj.relax.play();
+	    }
+	}, 10000);
+    }
+
+
+    obj.waiting = true;
+    obj.touched = false;
+    
+    //if the task is urgent, it must happen within 30 seconds
+    if (urgency=='urgent') {
+	setTimeout(function() {
+	    check(obj);
+	}, 30000);
     }
 }
 
@@ -170,7 +163,7 @@ function touch(obj) {
     _LTracker.push({'session': sessionID,'event': description,'score': score,});
     if (obj.waiting) {
 	obj.touched = true;
-	checkTouch(obj);
+	check(obj);
     }
 }
 
@@ -180,38 +173,24 @@ function falseTouch(name) {
     _LTracker.push({'session': sessionID,'event': s,'score': score,});
 }
 
-//check an object because it's been touched
-function checkTouch(obj) {
-    //if it's not waiting, we don't even care
+//check an object because it's been touched or because time is up
+function check(obj) {
+    //if it's not waiting, we don't even care (we also always expect it to be waiting when we check)
     if (obj.waiting) {
-	var objList = obj.siblings;
-	for (var i=0;i<objList.length;i++) {
-	    //if any of them were not touched, return and don't worry about going farther
-	    if (!objList[i].touched) {
-		return;
-	    }
+	//if it's been touched, then you get points
+	if (obj.touched) {
+	    var status = 'completeTask_' + obj.name;
+	    _LTracker.push({'session': sessionID,'event': status,'score': score,});
+	    goodJob.play();
+	    score = score + 10;
+	    $('#canvas').trigger('updateScore', score);
+	} else {
+	    var status = 'failTask_' + obj.name;
+	    _LTracker.push({'session': sessionID,'event': status,'score': score,});
+	    tooLate.play();
 	}
-	//if we get to the end and they were all touched, give the user points, and make them all stop waiting
-	_LTracker.push({'session': sessionID,'event': 'finishTask','score': score,});
-	goodJob.play();
-	score = score + 10;
-	$('#canvas').trigger('updateScore', score);
-	for (var i=0;i<objList.length;i++) {
-	    objList[i].waiting = false;
-	}
-    }
-}
-
-//check an object list because time is up
-function checkTimeout(objList) {
-    //if the objects are still waiting, then it means the person was too late
-    //if any are waiting, they should all be waiting
-    if (objList[0].waiting) {
-	_LTracker.push({'session': sessionID,'event': 'failTask','score': score,});
-	tooLate.play();
-	for (var i=0;i<objList.length;i++) {
-	    objList[i].waiting = false;
-	}
+	obj.waiting = false;
+	obj.touched = false;
     }
 }
 
